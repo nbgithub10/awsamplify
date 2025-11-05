478e06db04a07dfd436cbd791117f4a42c992e71
Nishant Bhatia <bhatia.nishant@gmail.com>
2025-11-05 16:31:01 +1100

    fetch all animals and display on page

---
+++ src/AnimalDisplay.js
@@ -0,0 +1,44 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function AnimalDisplay() {
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const response = await fetch('https://agus4uqwza.execute-api.ap-southeast-2.amazonaws.com/animals/9988');
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                const data = await response.json();
+                setPosts(data);
+                console.log(data);
+            } catch (err) {
+                setError(err);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchPosts();
+    }, []); // Empty dependency array means it runs once on mount
+
+    if (loading) return <div>Loading posts...</div>;
+    if (error) return <div>Error: {error.message}</div>;
+
+    return (
+        <div>
+            <h1>Posts</h1>
+            <ul>
+                {posts.map((post) => (
+                     <li key={post.animal_id}>{post.animal_id} - {post.address}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default AnimalDisplay;
--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import AnimalDisplay from "./AnimalDisplay";
 
 function App() {
   return (
     <div className="App">
+      <AnimalDisplay/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sourced from <code>App.js</code>. Reload to refresh.
+          Main container
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
