80c8f1a03e6861bb629aa0231438b6159199423e
poonam <poonamnbhatia@gmail.com>
2025-12-14 14:35:42 +1100

    search page modified

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import UserRegistration from './UserRegistration';
 import Home from './Home';
 import AnimalCare from './AnimalCare';
 import RealEstateHome from './RealEstateHome';
+import Search from './Search';
 
 function App() {
     const [currentView, setCurrentView] = useState('home'); // 'animal', 'csv', or 'profile'
@@ -22,6 +23,7 @@ function App() {
                 <Routes>
                     <Route path="/register" element={<UserRegistration />} />
                     <Route path="/animal-care" element={<AnimalCare />} />
+                    <Route path="/search" element={<Search />} />
                     <Route path="/real-estate-home" element={<RealEstateHome />} />
                     {/* original Home route kept at /home if needed */}
                     <Route path="/home" element={<Home />} />
